71637e7b92b20ce40eedb16ccfe4807e8e57296f
rheber <rheber@rediffmail.com>
2015-11-29 13:46:32 +1100

    sums

--- mentat.hs
+++ mentat.hs
@@ -6,12 +6,23 @@ import System.Random
 import System.Time
 
 -- Produces an infinite list of integers with d digits.
-randIntList :: Int -> IO [Int]
-randIntList d = getStdGen >>= return . randomRs (10^(d-1), (10^d)-1)
+randInts :: Int -> IO [Int]
+randInts d = getStdGen >>= return . randomRs (10^(d-1), (10^d)-1)
 
 -- Produces a list of n integers with d digits.
-someRandInts :: Int -> Int -> IO [Int]
-someRandInts n d = fmap (take n) (randIntList d)
+randIntsTake :: Int -> Int -> IO [Int]
+randIntsTake n d = fmap (take n) (randInts d)
+
+-- Produces a list of pairs of integers.
+randIntsTakePairs :: Int -> Int -> IO [(Int, Int)]
+randIntsTakePairs n d = do
+  aList <- randIntsTake (n * 2) d
+  let bList = drop n aList
+  return $ zip aList bList
+
+-- Counts how many items in a list are true.
+trueCount :: [Bool] -> Int
+trueCount = length . filter id
 
 -- Creates the text of a sum problem.
 sumProblemText :: Int -> Int -> Int -> String
@@ -19,18 +30,31 @@ sumProblemText a b d = "  " ++ (show a) ++
                      "\n+ " ++ (show b) ++
                      "\n==" ++ (replicate d '=')
 
+-- Prints a sum problem and examines an answer.
+sumProblem :: Int -> Int -> Int -> IO Bool
+sumProblem a b d = do
+  putChar '\n'
+  putStrLn $ sumProblemText a b d
+  answer <- getLine
+  return (a + b == read answer)
+
+-- Asks r sum problems.
+sumProblems :: Int -> Int -> IO [Bool]
+sumProblems r d = do
+  pairs <- randIntsTakePairs r d
+  let sp (a,b) = sumProblem a b d
+  sequence $ map sp pairs
+
 main :: IO ()
 main = do
   let digits = 3
-  numList <- someRandInts 2 digits
+  let reps = 5
 
   startTime <- getClockTime
-  let a = head numList
-  let b = numList !! 1
-  putStrLn $ sumProblemText a b digits
-  answer <- getLine
-  print (a + b == read answer)
+  results <- sumProblems reps digits
   endTime <- getClockTime
 
+  putStr "Correct answers: "
+  putStrLn $ (show $ trueCount results) ++ "/" ++ (show reps)
   putStr "Time taken: "
   putStrLn $ timeDiffToString $ diffClockTimes endTime startTime
